19ceccbcf40552260f808ec04e61eef30e914b33
ignis17 <joel.turbi@student.mec.cuny.edu>
2020-05-13 17:59:20 -0400

    main page

--- app/src/main/java/com/example/bankingapp/MainActivity.java
+++ app/src/main/java/com/example/bankingapp/MainActivity.java
@@ -3,17 +3,25 @@ package com.example.bankingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView titlepage, subtitlepage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         // target element
+        titlepage = findViewById(R.id.titlepage);
+        subtitlepage = findViewById(R.id.subtitlepage);
+
+
     }
 
     public void logout1(View view) {
